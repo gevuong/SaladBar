6de3bf5cb1b0491854c9ca47232f72edf4a5f738
George Vuong <georgevuong18@gmail.com>
2017-08-01 10:43:28 -0700

    create a canvas

---
+++ salad_bowl.js
@@ -0,0 +1,7 @@
+function saladBowl () {
+  this.x = width / 2; // starts at middle of window
+
+  this.show = function() {
+    rect(this.x, 20, 20, 20, 20);
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
-function setup() {
+// Using p5 JS framework which has a setup and draw function so it starts the program and draws loops for animation over and over again
 
+function setup() {
+  createCanvas(600, 400);
 }
 
 function draw() {
-  
+  background(51);
+  fill(255);
+  rect()
 }
