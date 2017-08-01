bcb5447729261a0a91e3e73df357e7fab9c7446e
George Vuong <georgevuong18@gmail.com>
2017-08-01 11:00:57 -0700

    object moves left right on screen

--- salad_bowl.js
+++ salad_bowl.js
@@ -2,6 +2,11 @@ function saladBowl () {
   this.x = width / 2; // starts at middle of window
 
   this.show = function() {
-    rect(this.x, 20, 20, 20, 20);
+    fill(255);
+    rect(this.x, height-20, 20, 20);
+  };
+
+  this.move = function(dir) {
+    this.x += dir;
   };
 }
--- sketch.js
+++ sketch.js
@@ -1,11 +1,21 @@
 // Using p5 JS framework which has a setup and draw function so it starts the program and draws loops for animation over and over again
+var bowl;
 
+// CONSTRUCTOR FUNCTION
 function setup() {
   createCanvas(600, 400);
+  bowl = new saladBowl();
 }
 
 function draw() {
   background(51);
-  fill(255);
-  rect()
+  bowl.show();
+}
+
+function keyPressed() {
+  if (keyCode === RIGHT_ARROW) {
+    bowl.move(1);
+  } else if (keyCode === LEFT_ARROW) {
+    bowl.move(-1);
+  }
 }
